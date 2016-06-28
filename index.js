76c3236dc5e8f30f2d06c3d0bacb369802ba5468
Sean Johnson <hello@seanjohnson.nz>
2016-06-28 15:13:31 +1200

    Updates

--- index.js
+++ index.js
@@ -14,4 +14,7 @@ xhr.get(endpoint, function (err, data) {
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: 'Space'})
+
+  var event = document.getElementsById("renderBtn")
+  event.addEventListener("click", greeting({render: 'DATA'}))
 })
