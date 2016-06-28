0a53badf78ecd6566501489786353b95886052ce
Sean Johnson <hello@seanjohnson.nz>
2016-06-28 16:06:41 +1200

    Updates

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ var greeting = require('./views/greeting.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-xhr.get(endpoint, function (err, data) {
-  if (err) {
-    console.error(err)
-  }
+var buttonClick = document.getElementById("renderBtn")
+buttonClick.addEventListener("click", renderData)
 
-  // In case you're curious
-  console.log("Here is the data: ", data.body) // FYI: data.body is a string
+function renderData() {
+  console.log("Button was clicked!")
+  var newTarget = document.getElementById('renderedData')
+  // newTarget.innerHTML = greeting({tryMe: 'Hello Everybody'})
+}
 
-  // Replace 'Space' below with the response
-  var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Space'})
+function refresh() {
 
-  function renderData () {
-    console.log("Button was clicked!")
-    console.log("Here is data object ", data)
-    var newTarget = document.getElementById('renderedData')
-    newTarget.innerHTML = greeting({tryMe: data.body})
-  }
+  xhr.get(endpoint, function (err, data) {
+    if (err) {
+      console.error(err)
+    }
 
-  var buttonClick = document.getElementById("renderBtn")
-  buttonClick.addEventListener("click", renderData)
+    // In case you're curious
+    console.log("Here is the data: ", data.body) // FYI: data.body is a string
 
+    // Replace 'Space' below with the response
+    var target = document.getElementsByTagName('main')[0]
+    target.innerHTML = greeting({name: 'Space'})
 
-})
+  })
+
+}
+
+refresh()
