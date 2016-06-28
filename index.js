ad8cab35da5a80ec943ddcf6aa62656b7d4d57ab
Sean Johnson <hello@seanjohnson.nz>
2016-06-28 16:15:44 +1200

    addEventListener Function working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
@@ -9,9 +9,21 @@ xhr.get(endpoint, function (err, data) {
   }
 
   // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
+  console.log("Here is the data: ", data.body) // FYI: data.body is a string
 
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: 'Space'})
+
+  function renderData () {
+    console.log("Button was clicked!")
+    console.log("Here is data object ", data)
+    var newTarget = document.getElementById('renderedData')
+    newTarget.innerHTML = data.body
+  }
+
+  var buttonClick = document.getElementById("renderBtn")
+  buttonClick.addEventListener("click", renderData)
+
+
 })
